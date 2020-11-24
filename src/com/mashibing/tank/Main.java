66f6e9a37c9dbd7af63d42eafa61e39701b60794
BeliveYourSelf <454886565@qq.com>
2020-11-24 16:44:39 +0800

    在主线程每隔50ms刷新窗口； 调用repaint()会自动调用，因为我们无法拿到画笔Graphics；

--- src/com/mashibing/tank/Main.java
+++ src/com/mashibing/tank/Main.java
@@ -10,9 +10,14 @@ import java.awt.event.WindowEvent;
  */
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		TankFrame tf = new TankFrame();
+		
+		while(true) {			// 让黑块自动移动
+			Thread.sleep(50);
+			tf.repaint();
+		}
 	}
 
 }
--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -2,6 +2,8 @@ package com.mashibing.tank;
 
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -15,7 +17,9 @@ public class TankFrame extends Frame{
 		setResizable(false);  // 固定窗口大小
 		setTitle("tank war"); // 设置标题栏名称
 		setVisible(true);     // 
-													 // 设置可以关闭窗体
+		
+		this.addKeyListener(new MyKeyListener());
+		// 设置可以关闭窗体
 		addWindowListener(new WindowAdapter() {    // WindowAdapter  继承 WindowListner
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -24,11 +28,28 @@ public class TankFrame extends Frame{
 		});
 	}
 	// 系统自动调用：当窗口需要重新绘制（打开窗口，移动窗口，关闭窗口。。。）
-	@Override
+	@Override 
 	public void paint(Graphics g) {
 		System.out.println("paint");
-		g.fillRect(x, y, 50, 50); // 在窗口画出一个黑方块
-		x +=10;                   // 通过移动框体，使得黑块移动
-		y +=10;
+		g.fillRect(x, y, 50, 50);
+		
+		x += 10;
+	}
+	
+	
+	class MyKeyListener extends KeyAdapter{
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println("keyPressed");
+			x +=200;
+			repaint();       //重新触发paint方法
+		}
+		
+		@Override
+		public void keyReleased(KeyEvent e) {
+			System.out.println("keyReleased");
+		}
+		
 	}
 }
