590f8919403068fd2e2eddfe90bdd1234b08dae4
BeliveYourSelf <454886565@qq.com>
2020-11-27 22:17:18 +0800

    new  出  5个敌人tank

--- src/com/mashibing/tank/Main.java
+++ src/com/mashibing/tank/Main.java
@@ -10,7 +10,10 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		TankFrame tf = new TankFrame();
-		
+		//初始化地方坦克
+		for (int i = 0; i < 5; i++) {
+			tf.tanks.add(new Tank(50 +i*60,200,Dir.DOWN,tf));
+		}
 		while(true) {			// 让黑块自动移动
 			Thread.sleep(50);
 			tf.repaint();
--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -10,9 +10,10 @@ import java.util.List;
 
 public class TankFrame extends Frame{
 
-	Tank myTank = new Tank(200,200,Dir.DOWN,this);
+	Tank myTank = new Tank(200,600,Dir.DOWN,this);
 
 	List<Bullet> bullets = new ArrayList<>();
+	List<Tank> tanks = new ArrayList<>();
 	Bullet bullet = new Bullet(300,300,Dir.DOWN,this);
 
 	static final int GAME_WIDTH =800, GAME_HEIGHT=600;
@@ -58,6 +59,9 @@ public class TankFrame extends Frame{
         for (int i=0;i<bullets.size();i++) {
             bullets.get(i).paint(g);
         }
+		for (int i = 0; i <tanks.size() ; i++) {
+			tanks.get(i).paint(g);
+		}
 	}
 
 
