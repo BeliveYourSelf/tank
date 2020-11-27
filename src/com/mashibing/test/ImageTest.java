9d13d393d20dbb40ba93857a4b18acd7d9a23856
BeliveYourSelf <454886565@qq.com>
2020-11-27 20:56:24 +0800

    Junit测试   从硬盘读取图片

---
+++ src/com/mashibing/test/ImageTest.java
@@ -0,0 +1,32 @@
+package com.mashibing.test;/**
+ * Created by Administrator on 2020/11/27 20:43
+ */
+
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Author Administrator
+ * @Description TODO
+ * Date 2020/11/27 20:43
+ * @Param
+ * @return
+ **/
+public class ImageTest {
+
+    @Test
+    void test(){
+        try{
+        BufferedImage image = ImageIO.read(new File("C:/Users/Administrator/Desktop/tank.jpg"));
+        assertNotNull(image); // 判断对象是否为空
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
