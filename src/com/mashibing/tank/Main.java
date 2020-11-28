7ddd6b1c0f5ce6161df952c3e5aa5596fff51902
BeliveYourSelf <454886565@qq.com>
2020-11-28 22:15:37 +0800

    加入配置文件，让程序更灵活

--- src/com/mashibing/tank/Main.java
+++ src/com/mashibing/tank/Main.java
@@ -10,8 +10,10 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		TankFrame tf = new TankFrame();
+
+		int initTankCount = Integer.parseInt((String)PropertyMgr.get("initTankCount"));
 		//初始化地方坦克
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < initTankCount; i++) {
 			tf.tanks.add(new Tank(50 +i*60,200,Dir.DOWN,Group.BAD,tf));
 		}
 //		new Thread(()->new Audio("audio/war1.wav").loop()).start();
