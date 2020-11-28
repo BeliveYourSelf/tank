44e3b5a8dab05afc2ef0537465be0e87e61f7ee8
BeliveYourSelf <454886565@qq.com>
2020-11-28 20:59:58 +0800

    好tank 和坏tank的图片区分

--- src/com/mashibing/tank/Tank.java
+++ src/com/mashibing/tank/Tank.java
@@ -17,8 +17,8 @@ public class Tank {
     private boolean live = true;
     private Dir dir = Dir.DOWN;
     private final int SPEED = 5;
-    public static int WIDTH =ResourceMgr.tankD.getWidth();
-    public static int HEIGHT =ResourceMgr.tankD.getHeight();
+    public static int WIDTH =ResourceMgr.goodTankD.getWidth();
+    public static int HEIGHT =ResourceMgr.goodTankD.getHeight();
     private TankFrame tf;
     private boolean moving = true;
     private Random random =new Random();
@@ -68,16 +68,16 @@ public class Tank {
         if(!live) tf.tanks.remove(this);
         switch (dir) {
             case LEFT:
-                g.drawImage(ResourceMgr.tankL,x,y,null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankL : ResourceMgr.badTankL,x,y,null);
                 break;
             case RIGHT:
-                g.drawImage(ResourceMgr.tankR,x,y,null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankR : ResourceMgr.badTankR,x,y,null);
                 break;
             case UP:
-                g.drawImage(ResourceMgr.tankU,x,y,null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankU : ResourceMgr.badTankU,x,y,null);
                 break;
             case DOWN:
-                g.drawImage(ResourceMgr.tankD,x,y,null);
+                g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankD : ResourceMgr.badTankD,x,y,null);
                 break;
         }
 
