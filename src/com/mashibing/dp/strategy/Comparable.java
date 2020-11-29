62c469b8b20f58358d9d6f64c2856b8a4c20d2e2
BeliveYourSelf <454886565@qq.com>
2020-11-29 23:00:34 +0800

    设计模式  工厂模式； bean工厂开头

--- src/com/mashibing/dp/strategy/Comparable.java
+++
@@ -1,8 +0,0 @@
-package com.mashibing.dp.strategy;
-
-/**
- * Created by Administrator on 2020/11/29 11:25
- */
-public interface Comparable<T> {
-    int compareTo(T o);
-}
--- src/com/mashibing/tank/Dir.java
+++
@@ -1,5 +0,0 @@
-package com.mashibing.tank;
-
-public enum Dir {
-	UP,DOWN,LEFT,RIGHT;
-}
